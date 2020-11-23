cb45669735cc7217e69203354e149a8b78372722
ChristophChang <info.chang@web.de>
2020-11-23 20:26:50 +0100

    Add some first basic main program sequence

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
 /*
+ *  Copyright (C) ChaLie    November 13 2020
+ * 
  *  Licensed under the Apache License, Version 2.0 (the "License"); you may
  *  not use this file except in compliance with the License.
  *  You may obtain a copy of the License at
@@ -21,6 +23,9 @@
 #include "app/Sensors.h"
 #include "app/LoraCommunication.h"
 #include "app/LicensePlateDisplay.h"
+#include "app/ParkingArea.h"
+
+#include "QRCode/src/qrcode.h"
 
 
 // Console 
@@ -33,38 +38,75 @@ FileHandle *mbed::mbed_override_console(int fd)
 
 int main()
 {    
-    Sensors sensorBoard;
-    sensorBoard.enable(AllSensors);
+    printf("LicensePlate2Tangle Project\r\n");
 
-//    LoraCommunication lora;
+    // Initialize the license plate components
+    ParkingArea parkingArea;
+    Sensors sensorBoard;
+    LoraCommunication lora;
     LicensePlateDisplay display;
-    
+
+    printf("Initializing Waveshare 4.2 Epaper display\r\n");
+    // Enable sensors to read data from
+    sensorBoard.enable(Temperature1);
     sensorBoard.read();
-    printf("\r\n");
-    sensorBoard.print();
-    printf("\r\n");
-    std::string sensorJson = sensorBoard.toJSON();
-    printf(sensorJson.c_str());
-    printf("\r\n");
-//    lora.sendMessage(sensorJson.c_str(), sensorJson.size());
-  
+
+    // Update the display information with the temperature value
+    // read from the sensor
+    display.setTemperature(sensorBoard.getValueFloat(Temperature1));
+
+    // For now the battery level is not supported, for now set it to full
+    display.setBatteryLevel(BatteryLevel::FULL);
+
+    // The QRCode will only be generated once
+    QRCode qrcode;
+    uint8_t *qrcodeData = new uint8_t[qrcode_getBufferSize(4)];
+    qrcode_initText(&qrcode, qrcodeData, 4, 0, "LicensePlate2Tangle,uid=E24F43FFFE44C3FC");   
+
+    display.setQRCode(&qrcode);
+
+    // On startup and show the welcome screen 
+    display.enable();
+    display.showWelcomeScreen();
+
+
+    // Now start up the lora communication module
+    printf("Initializing the Lora communication module\r\n");
+    lora.enable(); 
+
+    char messageBuffer[80];
+    size_t receivedBytes = 0;
+    
     while (true) {
 
-  
-      
-        sensorBoard.read();
-        printf("\r\n");
-        sensorBoard.print();
-        printf("\r\n");
+        // Now wait until the lora communication module is ready
+        if(LoraCommunication::Status::UP == lora.getStatus()) {
+                
+            // If ready regulary send sensor data to the server / iota tangle
 
-        std::string sensorJson = sensorBoard.toJSON();
-        printf(sensorJson.c_str());
-        printf("\r\n");
-        printf("size %d", sensorJson.size());
-        printf("\r\n");
+            // To save energy only enable the sensors for reading
+            std::string sensorJson = sensorBoard.toJSON();
+            lora.sendMessage((uint8_t*) sensorJson.c_str(), sensorJson.size());
+
+            // ALso if we want to see what was read from the sensors, put it on the console
+            printf("Read sensor data and try to send it to the server\r\n");
+            printf("Data: ");
+            printf(sensorJson.c_str());
+            printf("\r\n");
+
+            // Poll for incoming data
+            if(true == lora.receiveMessage((uint8_t*)messageBuffer, sizeof(messageBuffer), &receivedBytes)) {
+                
+                // Todo Now we have to parse the incoming Json message
+                // display.setLicense();
+                // display.setParkingEndTime();
+
+                // Finally there is booking, so lets show the customer
+                display.showParkingScreen();
+            }
+
+        }
         
-//        lora.sendMessage(sensorJson.c_str(), sensorJson.size());
-        
-        thread_sleep_for(5000);
+        thread_sleep_for(30000);
     }
 }
